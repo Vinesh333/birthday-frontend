50852730aa4d70614bad70008749db603817e971
Vinesh333 <vineshreddy333@gmail.com>
2025-06-18 18:06:52 +0530

    Added new album images

--- src/TinyClipsPage.js
+++ src/TinyClipsPage.js
@@ -13,6 +13,12 @@ function TinyClipsPage() {
     "video1.mp4",
     "video2.mp4",
     "video3.mp4",
+    "video4.mp4",
+    "video5.mp4",
+    "video6.mp4",
+    "video7.mp4",
+    "video8.mp4",
+    
   ];
 
   const handleVideoClick = (video) => {
